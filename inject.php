1bec23d5e925ac9e62e9796be0363147c943f35b
mkharis22 <55758771+mkharis22@users.noreply.github.com>
2019-12-13 10:20:53 +0700

    Update inject.php

--- inject.php
+++ inject.php
@@ -86,7 +86,7 @@ echo color("red","Masukan nomer: indonesia 62*****, Us 1*****\n");
         sleep(1);
         }
         sleep(3);
-        $boba19 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOODSENAG07"}');
+        $boba19 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOODSENANG07"}');
         $messageboba19 = fetch_value($boba19,'"message":"','"');
         if(strpos($boba19, 'Promo kamu sudah bisa dipakai.')){
         echo "\n".color("green","+] Message: ".$messageboba19);
